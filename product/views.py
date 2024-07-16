31056424d4d707d62cfc24b2f299baaf74dc8b16
Emanstone <emanstonepro@gmail.com>
2024-07-16 16:19:28 +0100

    Removed Django forms

--- product/views.py
+++ product/views.py
@@ -2,22 +2,22 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.views.generic import View
 from .models import Category, Product, Productimage
-from .forms import CategoryForm, ProductForm, ProductimageForm
+# from .forms import CategoryForm, ProductForm, ProductimageForm
 
 # Create your views here.
 
 class CategoryView(View):
     def get(self, request):
         categories = Category.objects.all()
-        form = CategoryForm()
-        return render(request, 'category_list.html', {'categories': categories, 'form': form})
+        # form = CategoryForm()
+        return render(request, 'category_list.html', {'categories': categories})
 
     def post(self, request):
-        form = CategoryForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('cat_list')
-        return render(request, 'category_list.html', {'form': form, 'categories': Category.objects.all()})
+        # form = CategoryForm(request.POST, request.FILES)
+        # if form.is_valid():
+        #     form.save()
+        #     return redirect('cat_list')
+        return render(request, 'category_list.html', {'categories': Category.objects.all()})
 
 
 
@@ -25,15 +25,15 @@ class CategoryView(View):
 class ProductView(View):
     def get(self, request):
         products = Product.objects.all(product_status='approved')
-        form = ProductForm()
-        return render(request, 'product_list.html', {'products': products, 'form': form})
+        # form = ProductForm()
+        return render(request, 'product_list.html', {'products': products})
 
     def post(self, request):
-        form = ProductForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('pro_list')
-        return render(request, 'product_list.html', {'form': form, 'products': Product.objects.all()})
+        # form = ProductForm(request.POST, request.FILES)
+        # if form.is_valid():
+        #     form.save()
+        #     return redirect('pro_list')
+        return render(request, 'product_list.html', {'products': Product.objects.all()})
 
 
 
@@ -41,12 +41,12 @@ class ProductView(View):
 class ProductimageView(View):
     def get(self, request):
         product_images = Productimage.objects.all()
-        form = ProductimageForm()
-        return render(request, 'productimage_list.html', {'product_images': product_images, 'form': form})
+        # form = ProductimageForm()
+        return render(request, 'productimage_list.html', {'product_images': product_images})
 
     def post(self, request):
-        form = ProductimageForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('proimage_list')
-        return render(request, 'productimage_list.html', {'form': form, 'product_images': Productimage.objects.all()})
+        # form = ProductimageForm(request.POST, request.FILES)
+        # if form.is_valid():
+        #     form.save()
+        #     return redirect('proimage_list')
+        return render(request, 'productimage_list.html', {'product_images': Productimage.objects.all()})
